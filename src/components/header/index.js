6968f6a6944d8d9e88d0de6b069a3060abec2a05
Robert Mccreary <fooyinfighter@gmail.com>
2022-10-24 18:34:05 -0700

    converted react classes to functions

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -1,13 +1,11 @@
 import React from 'react';
 
-class Header extends React.Component {
-  render() {
-    return (
-      <header>
-        <h1>RESTy</h1>
-      </header>
-    );
-  }
+function Header(props) {
+  return (
+    <header>
+      <h1>RESTy</h1>
+    </header>
+  );
 }
 
 export default Header;
