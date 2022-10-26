00e1095fceadb5ac83976f16b461d864a6984ddd
Robert Mccreary <fooyinfighter@gmail.com>
2022-10-26 09:05:17 -0700

    filesystem capitals

--- src/app.test.js
+++ src/app.test.js
@@ -1,7 +1,7 @@
 import {render, screen} from '@testing-library/react'
 import '@testing-library/jest-dom'
 import userEvent from '@testing-library/user-event'
-import App from './app';
+import App from './App';
 
 
 describe('Results', () => {
